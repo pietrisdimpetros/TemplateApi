d576deaa2175e491b2d898ccb675b0ccf7d41d92
CANDI\petros.p <petros.p@candi.gr>
2026-02-04 17:42:20 +0200

    bug fix

--- TemplateApi.Business/Constants/AuditConstants.cs
+++ TemplateApi.Business/Constants/AuditConstants.cs
@@ -2,7 +2,7 @@
 {
     public static class AuditConstants
     {
-        public const string Schema = nameof(ModuleSchemas.Audit);
+        public const string Schema = "audit";
         public const string Table = "Logs";
     }
 }
